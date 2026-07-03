98301eb0731ec55b32c71af30e822cd9ea44ff0f
Pedro <luispedro0810@gmail.com>
2026-07-02 21:19:10 -0300

    atividades

--- exercicios/ex009/__main__.py
+++ exercicios/ex009/__main__.py
@@ -1,8 +1,9 @@
 from ex009 import *
 
 def main():
-    pass
-
+    av1 = Avaliacao("Alana", "Português", 8.5)
+    av1.set_nota(7.4)
+    print(f"{av1.nome} tirou {av1.get_nota()} em {av1.disp}")
 if __name__ == "__main__":
     main()
     
